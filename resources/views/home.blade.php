2cc4a9156a73484988e0963dcf6c2f3ef783ffc3
nagachi <rafly.rigannagachi@gmail.com>
2021-06-15 20:16:20 +0700

    initial commit: add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/img/avatar.jpg" style="height: 150px; width:150px;" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>raflynagachi</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>342</strong> posts</div>
+                <div class="pr-5"><strong>73.5k</strong> followers</div>
+                <div class="pr-5"><strong>317</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Rafly Nagachi</div>
+            <div class="">
+                Halo gaes... gua nagachi, mari kita berteman.
+            </div>
+            <div><a href="#">nagachi.blog</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="/img/avatar.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/girl.png" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/boy.png" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
