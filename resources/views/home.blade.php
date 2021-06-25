651661309f344148d36ca5c8fe95b668458bb0c4
nagachi <rafly.rigannagachi@gmail.com>
2021-06-25 15:08:53 +0700

    add more view and adding eloquent logic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,17 +7,17 @@
             <img src="/img/avatar.jpg" style="height: 150px; width:150px;" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>raflynagachi</h1></div>
+            <div><h1>{{$user->username}}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>342</strong> posts</div>
                 <div class="pr-5"><strong>73.5k</strong> followers</div>
                 <div class="pr-5"><strong>317</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">Rafly Nagachi</div>
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
             <div class="">
-                Halo gaes... gua nagachi, mari kita berteman.
+                {{$user->profile->description}}
             </div>
-            <div><a href="#">nagachi.blog</a></div>
+            <div><a href="#">{{$user->profile->url}}</a></div>
         </div>
     </div>
     <div class="row pt-4">
